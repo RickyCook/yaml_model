b10c7353758e834e4f4e5ba7e87ad7f276042e67
Ricky Cook <rcook@odecee.com.au>
2015-02-01 14:11:49 +1100

    Base library, tests, etc

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name="yaml_model",
+      version="0.0.1",
+      description="Semi-transparent YAML serialization/deserialization",
+      author="Ricky Cook",
+      author_email="mail@thatpanda.com",
+      py_modules=['yaml_model'],
+      install_requires=['PyYAML>=3'],
+      )
