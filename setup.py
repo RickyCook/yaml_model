b06dc116756e42391a8c09a602f1108c88d3a38b
Ricky Cook <mail@thatpanda.com>
2015-02-01 15:53:42 +1100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name="yaml_model",
-      version="0.0.1",
+      version="0.1.0",
       description="Semi-transparent YAML serialization/deserialization",
       author="Ricky Cook",
       author_email="mail@thatpanda.com",
